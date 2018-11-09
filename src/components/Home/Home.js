e8071d2f15cb478efe8268063003b4411393179c
Nisha Gode <39167599+ngode1@users.noreply.github.com>
2018-11-09 14:37:17 -0500

    added fetch to Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,12 +5,51 @@ import FourColGrid from '../elements/FourColGrid/FourColGrid';
 import MovieThumb from '../elements/MovieThumb/MovieThumb';
 import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
 import Spinner from '../elements/Spinner/Spinner';
+import '../../config';
 import './Home.css';
+import { API_URL, API_KEY } from '../../config';
 
 class Home extends Component {
     state = {
-
+        movies: [],
+        heroImage: null,
+        loading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerm: '',
     }   
+
+    componentDidMount() {
+        this.setState({ loading: true })
+        const endpoint = `${API_URL}movie/popular?api_key${API_KEY}&language=en-US&page=1`;
+        this.fetchItems(endpoint)
+    }
+
+    loadMoreItems = () => {
+        let endpoint = '';
+        this.setState({ loading: true});
+
+        if (this.state.searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key${API_KEY}&language=en-US&page=${this.state.currenPage + 1}`;
+        } else {
+            endpoint = `${API_URL}movie/popular?api_key${API_KEY}&language=en-US&query${this.state.searchTerm}page=${this.state.currenPage + 1}`;
+        }
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result => {
+            this.setState({
+                movies: [...this.state.movies, ...result.results],
+                heroImage: this.state.heroImage || result.results[0],
+                loading: false,
+                currentPage: result.page,
+                totalPages: result.total_pages
+            })
+        })
+    }
 
     render(){
         return(
